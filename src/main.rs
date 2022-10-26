4e4bbdc5d93c410e7ddfea8b186e3ffb9b1fc652
Sergey Bargamon <sergey@bargamon.ru>
2022-10-26 14:09:58 +0200

    cleanup artists and albums

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,54 @@
 mod telegram_export;
 use telegram_export::{ImportData, Messages, Text};
+use convert_case::{Case, Casing};
+
+#[derive(Debug)]
+struct ExportData {
+    artist: Option<String>,
+    album: Option<String>,
+    tags: Vec<String>,
+}
+
+impl ExportData {
+    pub fn new() -> Self {
+        ExportData {
+            artist: None,
+            album: None,
+            tags: vec![],
+        }
+    }
+}
+
+fn clean_artist_name(mut hashtag: String) -> String {
+    if hashtag.starts_with('#'){
+        hashtag.remove(0);
+    }
+    hashtag.from_case(Case::Pascal).to_case(Case::Title)
+}
 
 fn main() {
     let import: ImportData = ImportData::from_file("./data/result.json");
     for message in import.messages {
         if let Messages::Message(user_message) = message {
-            let mut artist_and_album: String = String::new();
+            let mut export_data = ExportData::new();
             let mut has_links: bool = false;
             for text_entity in user_message.text_entities {
                 match text_entity {
                     Text::Hashtag(v) => {
-                        artist_and_album.push_str(&format!("{} ", v.text));
+                        let artist_name = clean_artist_name(v.text);
+                        if export_data.artist.is_none() {
+                            export_data.artist = Some(artist_name.clone());
+                        }
+                        export_data.tags.push(artist_name);
                     }
                     Text::Plain(v) => {
-                        if !artist_and_album.is_empty() && v.text.trim_start().starts_with('-') {
-                            let message_parts: Vec<&str> =
-                                v.text.trim_start().split('\n').collect();
+                        let text = v.text.trim_start();
+                        if export_data.artist.is_some() && text.starts_with('-') {
+                            let message_parts: Vec<&str> = text.split('\n').collect();
                             if let Some(album) = message_parts.first() {
-                                artist_and_album.push_str(album);
+                                let mut album_without_minus = album.to_string();
+                                album_without_minus.remove(0);
+                                export_data.album = Some(album_without_minus.trim().to_string());
                             }
                         }
                     }
@@ -29,8 +60,8 @@ fn main() {
                     }
                 }
             }
-            if !artist_and_album.is_empty() & has_links {
-                println!("{}", artist_and_album);
+            if export_data.artist.is_some() && has_links {
+                println!("{:?}", export_data);
             }
         }
     }
